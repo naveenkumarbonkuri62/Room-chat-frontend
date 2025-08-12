02a58f6b36bb24c9567c1120d22c057e46b4e34c
Naveen <naveenkumar.bonkurii@gmail.com>
2025-08-12 13:51:16 +0530

    room chat frontend v1.0

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:5459";
+export const baseURL = "https://room-chat-backend-ic7o.onrender.com";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
